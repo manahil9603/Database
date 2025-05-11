2363f3d736a11818251bc52e502ae2e6480a58d8
Manahil Hassan <manahilhassan9603@gmail.com>
2025-05-11 22:09:58 +0500

    Initial commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = 3000;
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
